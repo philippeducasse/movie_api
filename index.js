02c6d4a6c4b6ef9d7aba9d4f9c7aa737815f27dc
philippeducasse <ducassephi@hotmail.fr>
2023-05-01 12:32:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ app.use(cors({
 //  .then(() => { console.log('Connected to MongoDB'); }) .catch((err) => { console.error(err); });
 
 //this code is to connect to mongoAtlas
+console.log(process.env.PORT)
+console.log(process.env)
+console.log('Connection_URI:', process.env.CONNECTION_URI);
 
-console.log('Connection URI:', process.env.CONNECTION_URI);
-
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => { console.log('Connected to MongoDB'); }) .catch((err) => { console.error(err); });
 
 

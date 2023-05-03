eb8a368d85b5732ecffdf979b00f3cc6974e1077
philippeducasse <ducassephi@hotmail.fr>
2023-05-03 09:35:58 +0200

    allow access to all domains

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ const Users = Models.User;
 
 const app = express();
 const cors = require('cors');
+app.use(cors({ origin: '*' }))
 
 //code to restrict origin
 
 // let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 
-// app.use(cors({
+
 //     origin: (origin, callback)=> {
 //         if (!origin) return callback(null, true);
 //         if(allowedOrigins.indexOf(origin)=== -1 ) {//if specific origins isnt found on list of permitted origins
